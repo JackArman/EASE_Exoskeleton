7e0111c6f852d14789972b6da6bb4a6ef1df1362
Jennifer Yu <jeyu1717@gmail.com>
2025-10-06 19:04:41 +1100

    Add: refactor of control-systems1stworking for states

--- Tanmeet/MotorControl/MotorControl.ino
+++ Tanmeet/MotorControl/MotorControl.ino
@@ -2,24 +2,47 @@
 #include <mcp2515.h>
 #include <cstdint>
 
+
+const int BUTTON_PIN = 15;
+// assumes pull-up
+bool lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 200;
+
+// arbitary points to define our different stages
+int dataCheckpoints[2] = {51, 101};
+bool justReachedCheckpoint = false;
+
 struct can_frame canMsg;
 MCP2515 mcp2515(5);
 
+enum GaitState {
 
+  SWING_UP,
+  SWING_DOWN,
+  INITIAL
+};
 
+GaitState curState = INITIAL;
+
+// Common control params
+float v_des = 0.0;
+float kp = 40.0;
+float kd = 2.0;
+float torque_ff = 2.0;
+  
 #define GAIT_LENGTH 101
 double L_knee[GAIT_LENGTH] = {0.064467, 0.074331, 0.093322, 0.118156, 0.145765, 0.173782, 0.200732, 0.225926, 0.249164, 0.270381, 0.289373, 0.305691, 0.318736, 0.327958, 0.333073, 0.334175, 0.331732, 0.326490, 0.319331, 0.311129, 0.302627, 0.294319, 0.286371, 0.278621, 0.270692, 0.262185, 0.252879, 0.242834, 0.232383, 0.222038, 0.212359, 0.203835, 0.196767, 0.191192, 0.186847, 0.183235, 0.179761, 0.175892, 0.171276, 0.165752, 0.159294, 0.151928, 0.143711, 0.134770, 0.125387, 0.116065, 0.107502, 0.100468, 0.095640, 0.093459, 0.094056, 0.097284, 0.102846, 0.110498, 0.120221, 0.132296, 0.147250, 0.165738, 0.188439, 0.215986, 0.248939, 0.287736, 0.332620, 0.383527, 0.439959, 0.500893, 0.564828, 0.630043, 0.694954, 0.758340, 0.819280, 0.876881, 0.930024, 0.977325, 1.017316, 1.048739, 1.070786, 1.083212, 1.086280, 1.080587, 1.066864, 1.045814, 1.018047, 0.984064, 0.944284, 0.899057, 0.848651, 0.793217, 0.732766, 0.667199, 0.596433, 0.520656, 0.440760, 0.358855, 0.278567, 0.204591, 0.141522, 0.092671, 0.059562, 0.042007, 0.038405};
 double L_hip[GAIT_LENGTH] = {0.289862, 0.294106, 0.299675, 0.305515, 0.310784, 0.314967, 0.317822, 0.319227, 0.319036, 0.317008, 0.312815, 0.306110, 0.296648, 0.284423, 0.269742, 0.253211, 0.235617, 0.217761, 0.200304, 0.183673, 0.168035, 0.153324, 0.139305, 0.125649, 0.112010, 0.098116, 0.083820, 0.069120, 0.054131, 0.039038, 0.024051, 0.009396, -0.004705, -0.018052, -0.030488, -0.041909, -0.052281, -0.061665, -0.070250, -0.078398, -0.086628, -0.095527, -0.105598, -0.117101, -0.129972, -0.143842, -0.158155, -0.172330, -0.185914, -0.198666, -0.210577, -0.221803, -0.232542, -0.242877, -0.252683, -0.261621, -0.269205, -0.274901, -0.278197, -0.278642, -0.275855, -0.269508, -0.259313, -0.245034, -0.226550, -0.203951, -0.177646, -0.148385, -0.117162, -0.085016, -0.052805, -0.021057, 0.010050, 0.040595, 0.070755, 0.100649, 0.130239, 0.159319, 0.187544, 0.214489, 0.239725, 0.262917, 0.283915, 0.302772, 0.319662, 0.334727, 0.347929, 0.358969, 0.367321, 0.372359, 0.373525, 0.370504, 0.363396, 0.352850, 0.340090, 0.326739, 0.314464, 0.304605, 0.297928, 0.294529, 0.293879};
+
+
+// Note we are simply using the right knee/hip data arrays
 double R_knee[GAIT_LENGTH] = {0.039173, 0.059021, 0.085967, 0.115610, 0.144314, 0.169891, 0.191788, 0.210720, 0.227946, 0.244510, 0.260765, 0.276288, 0.290163, 0.301404, 0.309336, 0.313789, 0.315066, 0.313749, 0.310455, 0.305674, 0.299740, 0.292881, 0.285280, 0.277085, 0.268387, 0.259220, 0.249582, 0.239484, 0.228979, 0.218164, 0.207169, 0.196120, 0.185119, 0.174242, 0.163575, 0.153282, 0.143645, 0.135032, 0.127776, 0.122043, 0.117765, 0.114696, 0.112556, 0.111165, 0.110518, 0.110768, 0.112159, 0.114934, 0.119250, 0.125131, 0.132526, 0.141447, 0.152124, 0.165032, 0.180784, 0.199944, 0.222902, 0.249841, 0.280827, 0.315940, 0.355357, 0.399288, 0.447811, 0.500702, 0.557384, 0.616989, 0.678434, 0.740430, 0.801478, 0.859921, 0.914084, 0.962450, 1.003836, 1.037468, 1.062958, 1.080177, 1.089130, 1.089876, 1.082515, 1.067232, 1.044349, 1.014357, 0.977879, 0.935595, 0.888165, 0.836206, 0.780312, 0.721086, 0.659118, 0.594912, 0.528814, 0.461043, 0.391888, 0.322092, 0.253307, 0.188328, 0.130832, 0.084629, 0.052771, 0.036826, 0.036496};
 double R_hip[GAIT_LENGTH] = {0.330710, 0.336243, 0.342093, 0.346828, 0.349355, 0.349147, 0.346295, 0.341347, 0.335008, 0.327803, 0.319869, 0.310954, 0.300598, 0.288403, 0.274250, 0.258375, 0.241274, 0.223518, 0.205571, 0.187704, 0.170029, 0.152584, 0.135401, 0.118501, 0.101854, 0.085359, 0.068890, 0.052363, 0.035805, 0.019357, 0.003225, -0.012408, -0.027455, -0.041935, -0.055917, -0.069412, -0.082317, -0.094431, -0.105567, -0.115683, -0.124958, -0.133761, -0.142530, -0.151609, -0.161121, -0.170927, -0.180683, -0.189985, -0.198526, -0.206199, -0.213080, -0.219312, -0.224949, -0.229848, -0.233664, -0.235952, -0.236298, -0.234403, -0.230068, -0.223136, -0.213446, -0.200851, -0.185285, -0.166822, -0.145656, -0.122032, -0.096169, -0.068275, -0.038644, -0.007752, 0.023762, 0.055214, 0.086032, 0.115859, 0.144572, 0.172211, 0.198860, 0.224518, 0.249010, 0.271981, 0.292979, 0.311585, 0.327532, 0.340792, 0.351604, 0.360427, 0.367799, 0.374125, 0.379469, 0.383465, 0.385413, 0.384554, 0.380432, 0.373177, 0.363612, 0.353111, 0.343296, 0.335672, 0.331301, 0.330586, 0.333183};
 
-// #define GAIT_LENGTH 241
 
-// double L_hip[GAIT_LENGTH] = {-0.4457, -0.4467, -0.4467, -0.4461, -0.4452, -0.4442, -0.4416, -0.4384, -0.4351, -0.4317, -0.4279, -0.4230, -0.4170, -0.4097, -0.4012, -0.3918, -0.3822, -0.3727, -0.3629, -0.3532, -0.3438, -0.3344, -0.3255, -0.3172, -0.3101, -0.3049, -0.3005, -0.2970, -0.2934, -0.2898, -0.2868, -0.2843, -0.2825, -0.2818, -0.2820, -0.2831, -0.2853, -0.2882, -0.2917, -0.2961, -0.3013, -0.3069, -0.3129, -0.3190, -0.3255, -0.3315, -0.3377, -0.3440, -0.3503, -0.3564, -0.3621, -0.3671, -0.3712, -0.3746, -0.3776, -0.3807, -0.3838, -0.3872, -0.3908, -0.3948, -0.3990, -0.4035, -0.4086, -0.4139, -0.4192, -0.4243, -0.4290, -0.4332, -0.4371, -0.4414, -0.4455, -0.4497, -0.4540, -0.4580, -0.4623, -0.4665, -0.4705, -0.4746, -0.4788, -0.4831, -0.4870, -0.4908, -0.4947, -0.4984, -0.5020, -0.5057, -0.5090, -0.5126, -0.5160, -0.5192, -0.5227, -0.5260, -0.5292, -0.5323, -0.5351, -0.5376, -0.5401, -0.5422, -0.5442, -0.5460, -0.5473, -0.5484, -0.5492, -0.5498, -0.5501, -0.5504, -0.5505, -0.5504, -0.5502, -0.5500, -0.5496, -0.5491, -0.5486, -0.5480, -0.5471, -0.5462, -0.5451, -0.5436, -0.5416, -0.5391, -0.5361, -0.5325, -0.5284, -0.5234, -0.5181, -0.5122, -0.5055, -0.4983, -0.4905, -0.4827, -0.4745, -0.4659, -0.4571, -0.4478, -0.4382, -0.4287, -0.4187, -0.4088, -0.3986, -0.3887, -0.3792, -0.3702, -0.3619, -0.3540, -0.3468, -0.3400, -0.3340, -0.3283, -0.3232, -0.3182, -0.3134, -0.3087, -0.3040, -0.2994, -0.2946, -0.2897, -0.2847, -0.2796, -0.2745, -0.2694, -0.2642, -0.2589, -0.2536, -0.2483, -0.2431, -0.2378, -0.2325, -0.2272, -0.2217, -0.2159, -0.2104, -0.2052, -0.2001, -0.1949, -0.1895, -0.1843, -0.1793, -0.1742, -0.1691, -0.1640, -0.1587, -0.1534, -0.1478, -0.1423, -0.1370, -0.1321, -0.1276, -0.1235, -0.1197, -0.1166, -0.1141, -0.1121, -0.1105, -0.1091, -0.1077, -0.1062, -0.1044, -0.1026, -0.1009, -0.0991, -0.0975, -0.0963, -0.0955, -0.0949, -0.0946, -0.0946, -0.0949, -0.0954, -0.0961, -0.0971, -0.0984, -0.1000, -0.1017, -0.1037, -0.1058, -0.1081};
-// double L_knee[GAIT_LENGTH] = {-0.3011, -0.3086, -0.3234, -0.3309, -0.3456, -0.3619, -0.3753, -0.3907, -0.4053, -0.4192, -0.4323, -0.4452, -0.4573, -0.4688, -0.4795, -0.4891, -0.4984, -0.5065, -0.5135, -0.5195, -0.5239, -0.5280, -0.5310, -0.5330, -0.5342, -0.5345, -0.5346, -0.5334, -0.5319, -0.5302, -0.5282, -0.5263, -0.5246, -0.5234, -0.5226, -0.5224, -0.5225, -0.5233, -0.5243, -0.5259, -0.5278, -0.5301, -0.5327, -0.5355, -0.5386, -0.5416, -0.5445, -0.5472, -0.5496, -0.5516, -0.5532, -0.5544, -0.5552, -0.5556, -0.5560, -0.5562, -0.5565, -0.5570, -0.5578, -0.5588, -0.5601, -0.5617, -0.5636, -0.5657, -0.5681, -0.5705, -0.5729, -0.5752, -0.5772, -0.5788, -0.5801, -0.5810, -0.5815, -0.5820, -0.5825, -0.5829, -0.5834, -0.5839, -0.5847, -0.5857, -0.5870, -0.5885, -0.5905, -0.5927, -0.5952, -0.5978, -0.6008, -0.6038, -0.6069, -0.6099, -0.6128, -0.6155, -0.6180, -0.6201, -0.6220, -0.6236, -0.6247, -0.6256, -0.6262, -0.6266, -0.6269, -0.6270, -0.6269, -0.6268, -0.6265, -0.6261, -0.6256, -0.6251, -0.6245, -0.6239, -0.6233, -0.6226, -0.6215, -0.6200, -0.6180, -0.6158, -0.6133, -0.6105, -0.6074, -0.6040, -0.6002, -0.5962, -0.5919, -0.5872, -0.5824, -0.5772, -0.5719, -0.5663, -0.5607, -0.5550, -0.5492, -0.5433, -0.5377, -0.5322, -0.5268, -0.5218, -0.5173, -0.5130, -0.5090, -0.5053, -0.5019, -0.4990, -0.4965, -0.4944, -0.4925, -0.4908, -0.4891, -0.4874, -0.4856, -0.4836, -0.4813, -0.4787, -0.4759, -0.4730, -0.4700, -0.4670, -0.4639, -0.4608, -0.4578, -0.4548, -0.4519, -0.4490, -0.4462, -0.4435, -0.4407, -0.4379, -0.4350, -0.4320, -0.4289, -0.4258, -0.4227, -0.4196, -0.4165, -0.4135, -0.4107, -0.4081, -0.4057, -0.4036, -0.4019, -0.4004, -0.3992, -0.3983, -0.3976, -0.3972, -0.3971, -0.3971, -0.3973, -0.3974, -0.3974, -0.3974, -0.3972, -0.3968, -0.3961, -0.3952, -0.3939, -0.3923, -0.3905, -0.3884, -0.3860, -0.3832, -0.3802, -0.3768, -0.3732, -0.3695, -0.3655, -0.3615, -0.3574, -0.3531, -0.3491, -0.3466, -0.3439, -0.3418, -0.3406, -0.3397, -0.3389, -0.3377};
-
-// Assumes all gait arrays are the same length for simplicity
-// int gaitLength = 101;
+int LgaitIndex = 0;
+int RgaitIndex = 51;
 
 
 
@@ -73,191 +96,85 @@ void setup() {
   delay(10000);
   Serial.println("Multi-joint gait tracking started");
 }
+void loopIdle() {
+  // set into free move 
 
-void loop() {
-  // Common control params
-  float v_des = 0.0;
-  float kp = 40.0;
-  float kd = 2.0;
-  float torque_ff = 2.0;
-  
-  delay(10);
+}
 
-  int LgaitIndex = 0;
-  int RgaitIndex = 51;
-  for (int i = 1; i < 20; i++) {
-    sendMITCommand(-(R_hip[LgaitIndex] + 0.02) * i/20,  v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_HIP);
-    sendMITCommand(-(R_knee[LgaitIndex] * .8) * i/20, v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_KNEE);
-    sendMITCommand((R_hip[RgaitIndex] + 0.02) * i/20, v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_HIP);
-    sendMITCommand((R_knee[RgaitIndex] * .8) * i/20,v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_KNEE);
-    delay(50);
-  }  
+// setup to first position
+int i = 0;
+void loopInitial() {
+  sendMITCommand(-(R_hip[LgaitIndex] + 0.02) * i/20,  v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_HIP);
+  sendMITCommand(-(R_knee[LgaitIndex] * .8) * i/20, v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_KNEE);
+  sendMITCommand((R_hip[RgaitIndex] + 0.02) * i/20, v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_HIP);
+  sendMITCommand((R_knee[RgaitIndex] * .8) * i/20,v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_KNEE);
+  i++;
+  if (i == 20) {
+    // keep at datapoint 19 until state transitions
+    justReachedCheckpoint = true;
+    i = 19;
+  }
 
-  while (true) {
+}
+
+void loopData() {
     sendMITCommand(-(R_hip[LgaitIndex] + 0.02),  v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_HIP);
     sendMITCommand(-(R_knee[LgaitIndex] * .8), v_des, kp, kd, torque_ff, MOTOR_ID_LEFT_KNEE);
     sendMITCommand((R_hip[RgaitIndex] + 0.02), v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_HIP);
     sendMITCommand((R_knee[RgaitIndex] * .8),v_des, kp, kd, torque_ff, MOTOR_ID_RIGHT_KNEE);
-    LgaitIndex = (LgaitIndex + 1) % GAIT_LENGTH;
-    RgaitIndex = (RgaitIndex + 1) % GAIT_LENGTH;
-    delay(50); // ~50 Hz control loop
-  }
 
-  delay(200); // ~50 Hz control loop
 }
 
+void loop() {
+  // check signal - TODO replace with button or sensor reading
+  /*
+    int reading = digitalRead(BUTTON_PIN);
+    if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
 
-// // // #include <SPI.h>
-// // // #include <mcp2515.h>
+  if ((millis() - lastDebounceTime) > debounceDelay && reading == LOW && lastButtonState == HIGH) {
+    // Cycle to next state
+    currentState = (SystemState)((currentState + 1) % 3); // cycles through 3 states
 
-// // // struct can_frame canMsg;
-// // // MCP2515 mcp2515(5);
+  }
+  lastButtonState = reading;
 
-// // // // Gait cycle hip angles (radians, ~0–100% gait cycle)
-// // // // Sampled from typical sagittal hip flexion-extension profile
-// // // float gaitData[] = {
-// // //     0.307191696, 0.320702597, 0.335723251, 0.350966037, 0.365628158, 0.379334908, 0.391977391,
-// // //     0.403520446, 0.413852649, 0.422730594, 0.429827527, 0.434845055, 0.437627929, 0.438227203,
-// // //     0.436898228, 0.434046333, 0.430151173, 0.425689167, 0.421063756, 0.41654367, 0.412219626,
-// // //     0.408003419, 0.403689433, 0.399061424, 0.393998356, 0.388533416, 0.382847828, 0.377219404,
-// // //     0.371953561, 0.367315923, 0.363470992, 0.360437629, 0.358073758, 0.356108859, 0.354218565,
-// // //     0.352114014, 0.349602545, 0.346597338, 0.343083466, 0.339076288, 0.334605936, 0.329741218,
-// // //     0.324636475, 0.319565118, 0.314906373, 0.311079509, 0.308452884, 0.307266283, 0.307591267,
-// // //     0.309347274, 0.312373349, 0.316536437, 0.3218264, 0.328395731, 0.336531098, 0.34658972,
-// // //     0.358939819, 0.373926738, 0.39185461, 0.412962421, 0.437381501, 0.46507719, 0.495778572,
-// // //     0.528929588, 0.56371351, 0.599193599, 0.634507974, 0.668992761, 0.702147184, 0.733484913,
-// // //     0.762397478, 0.788131048, 0.809888132, 0.826983568, 0.838978467, 0.845738828, 0.847407798,
-// // //     0.844310707, 0.836844472, 0.825392719, 0.810285893, 0.791797457, 0.770155346, 0.74554977,
-// // //     0.718126608, 0.687968003, 0.655079803, 0.619408207, 0.580907836, 0.539681668, 0.496214447,
-// // //     0.451654425, 0.407973613, 0.367727353, 0.333414805, 0.306837898, 0.288824631, 0.279273928,
-// // //     0.277314439
-// // // };
-// // // int gaitLength = sizeof(gaitData) / sizeof(gaitData[0]);
-// // // int gaitIndex = 0;
+  below can simply add justReacheCheckpoint && buttonRead) for a combination
+  */
+  if (justReachedCheckpoint) {
+    // logic should switch between swing up and swing down states toggle between and 0 1
+    curState = (GaitState)((curState + 1) % 2);
+    Serial.print("Switched to state: ");
+    Serial.println(curState);
+    justReachedCheckpoint = true;
+  }
+  // 
 
-// // // // Float to uint conversion
-// // // int float_to_uint(float x, float x_min, float x_max, unsigned int bits) {
-// // //   float span = x_max - x_min;
-// // //   float offset = x_min;
-// // //   if (x < x_min) x = x_min;
-// // //   if (x > x_max) x = x_max;
-// // //   return (int)((x - offset) * ((float)((1 << bits) - 1) / span));
-// // // }
+  switch (curState) {
+    case INITIAL:
+      loopInitial();
+      break;
+    case SWING_UP:
+      loopData();
+      if (LgaitIndex != dataCheckpoints[0]) {
+        LgaitIndex = (LgaitIndex + 1) % GAIT_LENGTH;
+        RgaitIndex = (RgaitIndex + 1) % GAIT_LENGTH;    
+      } else {
+        justReachedCheckpoint = true;
+      }
+      break;
 
-// // // // Send MIT control packet
-// // // void sendMITCommand(float p_des, float v_des, float kp, float kd, float t_ff, uint8_t motor_id) {
-// // //   int p_int  = float_to_uint(p_des, -12.56f, 12.56f, 16);
-// // //   int v_int  = float_to_uint(v_des, -33.0f, 33.0f, 12);
-// // //   int kp_int = float_to_uint(kp, 0.0f, 500.0f, 12);
-// // //   int kd_int = float_to_uint(kd, 0.0f, 5.0f, 12);
-// // //   int t_int  = float_to_uint(t_ff, -65.0f, 65.0f, 12);
-
-// // //   uint32_t can_id = (0x08 << 8) | motor_id;
-// // //   canMsg.can_id = 0x80000000UL | can_id;
-// // //   canMsg.can_dlc = 8;
-
-// // //   canMsg.data[0] = kp_int >> 4;
-// // //   canMsg.data[1] = ((kp_int & 0xF) << 4) | (kd_int >> 8);
-// // //   canMsg.data[2] = kd_int & 0xFF;
-// // //   canMsg.data[3] = p_int >> 8;
-// // //   canMsg.data[4] = p_int & 0xFF;
-// // //   canMsg.data[5] = v_int >> 4;
-// // //   canMsg.data[6] = ((v_int & 0xF) << 4) | (t_int >> 8);
-// // //   canMsg.data[7] = t_int & 0xFF;
-
-// // //   mcp2515.sendMessage(&canMsg);
-// // // }
-
-// // // void setup() {
-// // //   Serial.begin(115200);
-// // //   SPI.begin();
-
-// // //   mcp2515.reset();
-// // //   mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ);
-// // //   mcp2515.setNormalMode();P51322,
-
-// #include <SPI.h>
-// #include <mcp2515.h>
-
-// struct can_frame canMsg;
-// MCP2515 mcp2515(5); // CS pin
-
-// // Converts float to unsigned int in range
-// int float_to_uint(float x, float x_min, float x_max, unsigned int bits) {
-//   float span = x_max - x_min;
-//   if (x < x_min) x = x_min;
-//   if (x > x_max) x = x_max;
-//   return (int)((x - x_min) * ((float)((1 << bits) - 1) / span));
-// }
-
-// void sendMITCommand(float p_des, float v_des, float kp, float kd, float t_ff, uint8_t motor_id) {
-//   int p_int  = float_to_uint(p_des, -12.56f, 12.56f, 16);
-//   int v_int  = float_to_uint(v_des, -33.0f, 33.0f, 12);
-//   int kp_int = float_to_uint(kp, 0.0f, 500.0f, 12);
-//   int kd_int = float_to_uint(kd, 0.0f, 5.0f, 12);
-//   int t_int  = float_to_uint(t_ff, -65.0f, 65.0f, 12);
-
-//   uint32_t can_id = (0x08 << 8) | motor_id;
-//   canMsg.can_id = 0x80000000UL | can_id;
-//   canMsg.can_dlc = 8;
-
-//   canMsg.data[0] = kp_int >> 4;
-//   canMsg.data[1] = ((kp_int & 0xF) << 4) | (kd_int >> 8);
-//   canMsg.data[2] = kd_int & 0xFF;
-//   canMsg.data[3] = p_int >> 8;
-//   canMsg.data[4] = p_int & 0xFF;
-//   canMsg.data[5] = v_int >> 4;
-//   canMsg.data[6] = ((v_int & 0xF) << 4) | (t_int >> 8);
-//   canMsg.data[7] = t_int & 0xFF;
-
-//   mcp2515.sendMessage(&canMsg);
-// }
-
-
-// void setup() {
-//   Serial.begin(115200);
-//   SPI.begin();
-
-//   mcp2515.reset();
-//   mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ);  // Match your CAN speed and crystal
-//   mcp2515.setNormalMode();
-
-//   Serial.println("CAN sniffer started...");
-//   Serial.println("Sending ±6 degree test command");
-
-//   float angle_radians = 6.0 * PI / 180.0; // 6 degrees → radians = 0.1047
-//   float v_des = 0.0;
-//   float kp = 40.0;
-//   float kd = 2.0;
-//   float torque_ff = 0.0;
-//   uint8_t motor_id = 0x68; // change to your motor ID
-
-//   sendMITCommand(angle_radians, v_des, kp, kd, torque_ff, motor_id);
-
-//   delay(1000); // wait 1s
-
-//   sendMITCommand(-angle_radians, v_des, kp, kd, torque_ff, motor_id);
-
-//   Serial.println("Done.");
-// }
-
-// void loop() {
-//   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-//     // Extract standard CAN ID (ignore extended flag bits)
-//     uint32_t can_id = canMsg.can_id & 0x1FFFFFFF;
-
-//     Serial.print("CAN ID: 0x");
-//     Serial.print(can_id, HEX);
-//     Serial.print(" | Data: ");
-
-//     for (int i = 0; i < canMsg.can_dlc; i++) {
-//       if (canMsg.data[i] < 0x10) Serial.print("0");
-//       Serial.print(canMsg.data[i], HEX);
-//       Serial.print(" ");
-//     }
-
-//     Serial.println();
-//   }
-// }
-
-
+    case SWING_DOWN:
+      loopData();
+      if (LgaitIndex != dataCheckpoints[1]) {
+        LgaitIndex = (LgaitIndex + 1) % GAIT_LENGTH;
+        RgaitIndex = (RgaitIndex + 1) % GAIT_LENGTH;      
+      } else {
+        justReachedCheckpoint = true;
+      }
+      break;
+  }
+  // adjust delay if needed
+  delay(200); // ~50 Hz control loop
+}
